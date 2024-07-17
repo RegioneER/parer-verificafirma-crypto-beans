a7637987f2276573ce74db372dac8c077a23c989
Stefano Sinatti <stefano.sinatti@regione.emilia-romagna.it>
2024-07-17 18:02:24 +0200

    Release 1.4.1 (#4)
    * [skip ci] Update project to latest tag verificafirma-crypto-beans-1.4.1
    
    * Update pom.xml
    
    * [maven-release-plugin] prepare release verificafirma-crypto-beans-1.4.1
    
    * [maven-release-plugin] prepare for next development iteration
    
    ---------
    
    Co-authored-by: parerworker <DevPARER@regione.emilia-romagna.it>
    Co-authored-by: GitHub Actions <actions@github.com>

--- src/main/java/it/eng/parer/crypto/model/exceptions/ParerError.java
+++ src/main/java/it/eng/parer/crypto/model/exceptions/ParerError.java
@@ -35,32 +35,31 @@ public interface ParerError {
      * Ambito dell'eccezione, sostanzialmente uno per <i>service</i> implementato.
      */
     public enum ExceptionType {
-        PARAMETER_VALIDATION, GENERIC, NOT_FOUND, CA, CERTIFICATE, CRL, TIME, SIGNATURE_VERIFICATION,
-        SIGNATURE_EXTRACTION
+        GENERIC, TSD, CERTIFICATE, CRL, TIME
     }
 
     /**
      * Codici di errore specifici che vanno a dettagliare l'{@link ExceptionType}.
      */
     public enum ErrorCode {
-        RESOURCE_NOT_FOUND("not-found-exception", ExceptionType.NOT_FOUND), //
-        VALIDATION_ERROR("validation-exception", ExceptionType.PARAMETER_VALIDATION), //
+        RESOURCE_NOT_FOUND("not-found-exception", ExceptionType.GENERIC), //
+        VALIDATION_ERROR("validation-exception", ExceptionType.GENERIC), //
         GENERIC_ERROR("unhandled-exception", ExceptionType.GENERIC), //
-        CRL_NOT_FOUND("crl-not-found-exception", ExceptionType.NOT_FOUND), //
+        CRL_NOT_FOUND("crl-not-found-exception", ExceptionType.TSD), //
         CRL_CRYPTO_STORAGE("crl-cryptostorage-exception", ExceptionType.CRL), //
         CRL_EXCEPTION("crl-exception", ExceptionType.CRL), //
         CRL_IO("crl-io-exception", ExceptionType.CRL), //
         TSP_EXCEPTION("tsp-exception", ExceptionType.TIME), //
         TSP_IO("tsp-io-exception", ExceptionType.TIME), //
         TSP_PROVIDER_ERROR("tsp-provider-exception", ExceptionType.TIME), //
-        TSD_NOT_FOUND("tsd-not-found-exception", ExceptionType.NOT_FOUND), //
+        TSD_NOT_FOUND("tsd-not-found-exception", ExceptionType.TSD), //
         TSD_PROVIDER_ERROR("tsd-provider-exception", ExceptionType.TIME), //
         CERT_IO("cert-io-exception", ExceptionType.CERTIFICATE), //
         CERT_EXCEPTION("cert-exception", ExceptionType.CERTIFICATE), //
         CERT_PROVIDER_ERROR("cert-provider-exception", ExceptionType.CERTIFICATE), //
-        SIGNATURE_VERIFICATION_IO("signature-io-exception", ExceptionType.SIGNATURE_VERIFICATION), //
-        SIGNATURE_WRONG_PARAMETER("signature-wrong-parameters", ExceptionType.SIGNATURE_VERIFICATION),
-        SIGNATURE_FORMAT("signature-format", ExceptionType.SIGNATURE_EXTRACTION);
+        SIGNATURE_VERIFICATION_IO("signature-io-exception", ExceptionType.GENERIC), //
+        SIGNATURE_WRONG_PARAMETER("signature-wrong-parameters", ExceptionType.GENERIC),
+        SIGNATURE_FORMAT("signature-format", ExceptionType.GENERIC);
 
         ErrorCode(String urlFriendly, ExceptionType type) {
             this.urlFriendly = urlFriendly;
